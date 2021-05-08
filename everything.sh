52c2e85332e6298db0e951ae00b8f438274f9914
Jnnngs <4107228+jnnngs@users.noreply.github.com>
2021-05-08 14:44:51 +0100

    added home page HTML

--- everything.sh
+++ everything.sh
@@ -51,7 +51,7 @@ function runBenchmark(){
 		### display main menu ###
 		dialog --backtitle "Benchmark" \
 		--title "Main Menu" \
-		--menu "Please select an option below." 15 65 8 \
+		--menu "Please select an option below." 15 60 8 \
 		USA "Benchmark & The US Speedtest" \
 		Europe "Benchmark & Europe Speedtest" \
 		MiddleEast  "Benchmark & Middle East Speedtest" \
@@ -160,7 +160,7 @@ do
 	### display main menu ###
 	dialog --backtitle "Mega awesome collection of everything.sh bash" \
 	--title "Main Menu" \
-	--menu "Please select an option below." 15 65 5 \
+	--menu "Please select an option below." 15 60 5 \
 	Benchmark "Perform a Benchmark test" \
 	Harden "Harden a newly installed Linux OS" \
 	RDP "Install Remote Desktop (xRDP)" \
